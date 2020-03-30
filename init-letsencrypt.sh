9ca59bbd76a557200ae15a3a77dfe4e4d249532c
rakib-09 <rakib.rumi09@gmail.com>
2020-03-30 22:34:19 +0600

    added gd spport for jpeg

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(example.org www.example.org)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./docker/certbot"
 email="rakib.rumi09@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
